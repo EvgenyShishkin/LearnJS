a469cecec00b06173f8840e20f1a614d5480bbd9
Evgeny Shishkin <jovanny090691@gmail.com>
2019-07-24 22:37:37 +0300

    Array find and change Max/Min' git starus git status

---
+++ JS.js
@@ -0,0 +1,30 @@
+var NumbersArr = [];
+
+function getRandomInt(min = 0, max = 100)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < 1000; i++) {
+  NumbersArr.push(getRandomInt());
+}
+let i = 0, min = i, max = i;
+while (i < 1000) {
+  if (NumbersArr[i] > NumbersArr[max]) {
+    max = i;
+  } 
+  if (NumbersArr[i] < NumbersArr[min]) {
+    min = i;
+  }
+  i++;
+  
+}
+console.log (max, NumbersArr[max]);
+console.log (min, NumbersArr[min]);
+
+var n = NumbersArr[max];
+NumbersArr[max] = NumbersArr[min];
+NumbersArr[min] = n;
+
+console.log (max, NumbersArr[max]);
+console.log (min, NumbersArr[min]);
