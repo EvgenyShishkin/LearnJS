e40393d6ffba39401290dc5bfc6cd95c5abdd9ea
Evgeny Shishkin <jovanny090691@gmail.com>
2019-07-27 13:03:53 +0300

    addCacl_Lesson_3

---
+++ JS_calc.js
@@ -0,0 +1,20 @@
+
+var num1 = 2,
+    num2 = 3,
+    answer = 0,
+    userVersion = 0;
+
+function getRandomInt(min = 0, max = 10)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+num1 = getRandomInt();
+num2 = getRandomInt();
+answer = num1*num2;
+userVersion = +prompt("Сколько будет " + num1 + "*" + num2 + "?");
+
+if (userVersion == answer) {
+  alert ("Это верно!");
+} else {
+  alert("Не верно! Правильный ответ " + answer);
+}
